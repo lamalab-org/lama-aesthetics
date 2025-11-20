096dd411c748f2a81f4171083b17bbd8d310619e
MrtinoRG <martinriosgarcia@gmail.com>
2025-11-20 15:27:25 +0100

    feat: move to src package + add dimension constants

---
+++ src/lama_aesthetics/__init__.py
@@ -0,0 +1,21 @@
+from lama_aesthetics.aesthetics import (
+    STYLES,
+    get_style,
+    ONE_COL_WIDTH_INCH,
+    TWO_COL_WIDTH_INCH,
+    ONE_COL_GOLDEN_RATIO_HEIGHT_INCH,
+    TWO_COL_GOLDEN_RATIO_HEIGHT_INCH,
+)
+from lama_aesthetics.plotutils import range_frame, ylabel_top, add_identity
+
+__all__ = [
+    "STYLES",
+    "get_style",
+    "range_frame",
+    "ylabel_top",
+    "add_identity",
+    "ONE_COL_WIDTH_INCH",
+    "TWO_COL_WIDTH_INCH",
+    "ONE_COL_GOLDEN_RATIO_HEIGHT_INCH",
+    "TWO_COL_GOLDEN_RATIO_HEIGHT_INCH",
+]
